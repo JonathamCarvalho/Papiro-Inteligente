8557976aedf35a870b97f5dae245ece3e7ecec28
JonathamCarvalho <jonatham.carvalho@gmail.com>
2025-04-23 12:54:56 -0300

    Ajusta rotas para deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Papiro-Inteligente/',
+  base: '/Papiro-Inteligente/', // Isso já está correto
+  build: {
+    outDir: 'dist', // Garantir que o diretório de build esteja correto
+    emptyOutDir: true, // Limpa o diretório antes de construir
+  },
 });
